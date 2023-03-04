83d04244e019c458fe1e9201243efd31cff955d1
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-03-04 05:41:28 +0000

    meshiterro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user,only:[:edit,:update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
   end
 
   def update
@@ -20,4 +22,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name,:profile_image)
   end
 
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  
 end
